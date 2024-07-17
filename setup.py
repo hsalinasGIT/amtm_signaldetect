1a2323dc974154ab07bc77af1d48e8a998acf800
Hector A Salinas <133439274+hsalinasGIT@users.noreply.github.com>
2024-07-17 16:53:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="amtm_signaldetect",
-    version="0.0.2",
+    version="0.0.1",
     description="Periodic Signal Detection via the Adaptive Multitaper Method (aMTM)",
     long_description=readme(),
     long_description_content_type="text/markdown",
